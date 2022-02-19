2f0787ea8924d9517ffee475467a123b9df86c13
Ahmad-Fahad <ahmadsmfahad@gmail.com>
2022-02-19 10:45:40 +0600

    Code_Archieve

--- Concept/Sand_Box.py
+++ Concept/Sand_Box.py
@@ -1,25 +1,24 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-    	n = len(nums)
-		current_index = 1
-		for i in range(1, n):
-			if nums[i] != nums[i-1]:
-				nums[current_index] = nums[i]
-				current_index += 1
-			else:
-				continue
-		return current_index 
+<div class='col-md-6 col-sm-6 single-contact-detail'>
+              <div class='header'>
+                <i class='lnr lnr-phone-handset'/>
+                <h5>TELEPHONE</h5>
+              </div>
+              <p>+88 01521 485483 <br/>+88 01521 485 483</p>
+            </div>
 
 
 
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        current_index = 1
-        nums[current_index-1] = nums[0]
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[current_index] = nums[i]
-                current_index += 1
-            else:
-                continue
-        return current_index
+
+
+        <div class='social-link' style='margin: 0;padding: 10px 0;text-align: center;'>
+                  <!-- About Social Links --> 
+                  <a href='https://www.facebook.com/Ahmad.Fahad.2008' target="_blank"><i class='fa fa-facebook'/></a>
+                    <a href='https://github.com/Ahmad-Fahad' target="_blank"><i class='fa fa-github'/></a>
+                    <a href='https://www.hackerrank.com/S_M_Fahad_Ahmad' target="_blank"><i class='fa fa-hackerrank'/></a>
+                    <a href='https://www.linkedin.com/in/engr-s-m-fahad-ahmad-1530b3145/' target="_blank"><i class='fa fa-linkedin'/></a>
+                </div>
+
+
+           
+
+           <span class='typist-text' data-typist='typist-blink' id='typist-element'>Computer Engineer</span>
